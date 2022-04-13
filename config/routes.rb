dc82279cad4d96adfaa1cc0843feaf7a1f571de5
pezholio <pezholio@gmail.com>
2022-04-13 12:52:40 +0100

    Remove template cruft

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   get "health_check" => "application#health_check"
-  root to: "visitors#index"
 
   # If the CANONICAL_HOSTNAME env var is present, and the request doesn't come from that
   # hostname, redirect us to the canonical hostname with the path and query string present
