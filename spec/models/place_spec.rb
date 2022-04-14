e307d1596261fe8cac5779b47ddebcd5965e3770
pezholio <pezholio@gmail.com>
2022-04-14 13:37:14 +0100

    Add `has_many` relation to Place

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Place, type: :model do
+  context "associations" do
+    it { should have_many(:votes) }
+  end
+
   it "should exist" do
     place = build(:place)
 
