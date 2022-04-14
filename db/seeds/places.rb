85100c7bef8e196ed53941a050053dc894144e39
pezholio <pezholio@gmail.com>
2022-04-14 11:49:39 +0100

    Add seeds for places
    This takes a corpus of the existing data, so we have
    something to work with. Because we want a reliable
    way to link votes and places, we’re keeping the
    original IDs. I’ve also added a `S3_HOSTNAME`
    env variable, so we have a reliable way of
    generating the cached images that we have
    saved in s3

---
+++ db/seeds/places.rb
@@ -0,0 +1,39 @@
+require "csv"
+
+places_filename = File.join(
+  Rails.root,
+  "db",
+  "data",
+  "development",
+  "places.csv"
+)
+
+CSV.foreach(places_filename, headers: true, encoding: "BOM|UTF-8") do |row|
+  geograph_id = row["geograph_uri"].split("/").last
+  image_code = row["image_uri"].split("/").last
+  image_uri = [ENV["S3_HOSTNAME"], image_code].join("/")
+
+  params = {
+    geograph_id: geograph_id,
+    title: row["title"],
+    description: row["description"],
+    subject: row["subject"],
+    creator: row["creator"],
+    creator_uri: row["creator_uri"],
+    date_submitted: DateTime.parse(row["date_submitted"]),
+    lat: row["lat"],
+    lon: row["lon"],
+    gridsquare: row["gridsquare"],
+    license_uri: row["license_uri"],
+    format: row["format"],
+    votes: row["votes"],
+    random: row["random"],
+    width: row["width"],
+    height: row["height"],
+    aspect: row["aspect"],
+    geograph_image_uri: row["image_uri"],
+    image_uri: image_uri
+  }
+
+  Place.find_or_create_by(id: row["id"].to_i).update!(params)
+end
