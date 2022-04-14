f259d9b8aa36898a6b7a234df7febdb7267fbf10
pezholio <pezholio@gmail.com>
2022-04-14 11:19:23 +0100

    Update settings for Rails 7.0
    This runs bin/rails app:update to update some defaults,
    as well as adding "sprockets-rails" to the Gemfile, and
    loading the default 7.0 config defaults. The tests run
    OK, so working on  the assumption that we’re on a
    greenfield app, so there should be minimal issues.

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem "puma", "~> 5.6"
 gem "rollbar"
 gem "rails", "~> 7.0.2"
 gem "sass-rails", "~> 6.0"
+gem "sprockets-rails"
 gem "turbolinks", "~> 5"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "uglifier", ">= 1.3.0"
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module ScenicOrNot
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
