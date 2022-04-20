9515898cc44836de6f08559b336e6df35786d752
Cristina <cristina@dxw.com>
2022-04-20 18:16:00 +0100

    Most and least scenic places
    I’ve ported the original queries to Postgres, but kept the principle of
    using raw SQL due to the complexity of the calculations.
    
    I’ve added a last resort option in case we don’t have enough data that
    fulfils the default requirements to make it into top and bottom 5
    respectively.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,32 @@ class Vote < ApplicationRecord
   belongs_to :place
 
   validates_uniqueness_of :uuid, scope: :place
+
+  def self.most_scenic_places
+    results = ActiveRecord::Base.connection.execute(top_query)
+    if results.entries.size < 5
+      results = ActiveRecord::Base.connection.execute(top_query(min_score: 5, min_vote_count: 1))
+    end
+    results.entries
+  end
+
+  def self.least_scenic_places
+    results = ActiveRecord::Base.connection.execute(bottom_query)
+    if results.entries.size < 5
+      results = ActiveRecord::Base.connection.execute(bottom_query(max_score: 5, min_vote_count: 1))
+    end
+    results.entries
+  end
+
+  def self.top_query(min_score: 9, min_vote_count: 3, num_places: 5)
+    "SELECT place_id, count(place_id) AS vote_count, avg(rating) AS score FROM votes " \
+    "GROUP BY place_id HAVING avg(rating) > #{min_score} AND count(place_id) >= #{min_vote_count} " \
+    "ORDER BY score DESC, vote_count DESC LIMIT #{num_places}"
+  end
+
+  def self.bottom_query(max_score: 1, min_vote_count: 3, num_places: 5)
+    "SELECT place_id, count(place_id) AS vote_count, avg(rating) AS score FROM votes " \
+    "GROUP BY place_id HAVING avg(rating) <= #{max_score} AND count(place_id) >= #{min_vote_count} " \
+    "ORDER BY score ASC, vote_count DESC LIMIT #{num_places}"
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -23,4 +23,42 @@ RSpec.describe Vote, type: :model do
       expect(place2_vote.valid?).to be_truthy
     end
   end
+
+  context "most and least scenic places" do
+    let(:most_scenic_place) { FactoryBot.create(:place) }
+    let(:least_scenic_place) { FactoryBot.create(:place) }
+
+    before do
+      FactoryBot.create_list(:vote, 3, place: most_scenic_place, rating: 10)
+      FactoryBot.create_list(:vote, 3, place: least_scenic_place, rating: 1)
+      FactoryBot.create_list(:vote, 5, rating: 8)
+      FactoryBot.create_list(:vote, 5, rating: 3)
+    end
+
+    describe ".most_scenic_places" do
+      it "returns the top 5 rated places" do
+        top_five = Vote.most_scenic_places
+
+        expect(top_five.size).to eql(5)
+        expect(top_five.first).to match(hash_including(
+          "place_id" => most_scenic_place.id,
+          "score" => 10,
+          "vote_count" => 3
+        ))
+      end
+    end
+
+    describe ".least_scenic_places" do
+      it "returns the bottom 5 rated places" do
+        bottom_five = Vote.least_scenic_places
+
+        expect(bottom_five.size).to eql(5)
+        expect(bottom_five.first).to match(hash_including(
+          "place_id" => least_scenic_place.id,
+          "score" => 1,
+          "vote_count" => 3
+        ))
+      end
+    end
+  end
 end
