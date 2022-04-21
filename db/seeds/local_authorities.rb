82322856193dc403fad35d105d3b9ab9719df514
Cristina <cristina@dxw.com>
2022-04-21 18:40:49 +0100

    Add seed for local authorities

---
+++ db/seeds/local_authorities.rb
@@ -0,0 +1,17 @@
+require "csv"
+
+councils_filename = File.join(
+  Rails.root,
+  "db",
+  "data",
+  "production",
+  "local_authorities.tsv"
+)
+
+CSV.foreach(councils_filename, headers: true, encoding: "BOM|UTF-8", col_sep: "\t") do |row|
+  params = {
+    name: row["name"]
+  }
+
+  LocalAuthority.find_or_create_by(name: row["name"].squish).update!(params)
+end
