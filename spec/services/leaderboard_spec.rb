27d2536a49638ba9ba1ddae5c2481796747e2ddd
Cristina <cristina@dxw.com>
2022-04-26 15:10:18 +0100

    Turn the leaderboard into a service
    The logic for the leaderboard is complex enough to deserve its own
    service class, rather than being a part of the `Vote` model.
    
    This choice also makes it easier to see at a glance the default options
    and their fallbacks.
    
    We’ve changed the options as follows:
    - maximum rating for bottom 5 is 2 by default, with 3 as a fallback[1]
    
    [1] Ratings tend to be quite stern, so even a score of 5 for a built-up
    area is a pretty good score.

---
+++ spec/services/leaderboard_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe Leaderboard, type: :service do
+  context "most and least scenic places" do
+    let(:most_scenic_place) { FactoryBot.create(:place) }
+    let(:least_scenic_place) { FactoryBot.create(:place) }
+
+    before do
+      FactoryBot.create_list(:vote, 3, place: most_scenic_place, rating: 10)
+      FactoryBot.create_list(:vote, 3, place: least_scenic_place, rating: 1)
+      FactoryBot.create_list(:vote, 5, rating: 8)
+      FactoryBot.create_list(:vote, 5, rating: 3)
+    end
+
+    describe "most_scenic_places" do
+      it "returns the top 5 rated places" do
+        top_five = Leaderboard.new.most_scenic_places
+
+        expect(top_five.size).to eql(5)
+        expect(top_five.first).to match(hash_including(
+          "place_id" => most_scenic_place.id,
+          "score" => 10,
+          "vote_count" => 3
+        ))
+      end
+    end
+
+    describe "least_scenic_places" do
+      it "returns the bottom 5 rated places" do
+        bottom_five = Leaderboard.new.least_scenic_places
+
+        expect(bottom_five.size).to eql(5)
+        expect(bottom_five.first).to match(hash_including(
+          "place_id" => least_scenic_place.id,
+          "score" => 1,
+          "vote_count" => 3
+        ))
+      end
+    end
+  end
+end
